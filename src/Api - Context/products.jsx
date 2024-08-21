4875d64a1a90447880d47870b28acc0b267d96a4
Velastorium <velacastanos@gmail.com>
2024-08-21 15:12:43 -0500

    Actualizar

--- src/Api - Context/products.jsx
+++
@@ -1,6 +0,0 @@
-const API = 'http://localhost:10000/fepi/items';
-const CATEGORY_API = 'http://localhost:10000/fepi/categories';
-
-export const getItemsRequest = () => axios.get(`${API}`);
-export const getItemByIdRequest = (id) => axios.get(`${API}/${id}`);
-export const getCategoriesRequest = () => axios.get(`${CATEGORY_API}`);
